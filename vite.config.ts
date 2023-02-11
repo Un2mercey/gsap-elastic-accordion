43272d400773c23b5164f55e5f24925831c1517a
Arseniy Markov <zamberg42@gmail.com>
2023-02-11 19:14:39 +0300

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/gsap-elastic-accordion/',
+    base: '/<repo>/',
     build: {
         outDir: resolve(__dirname, 'docs'),
     },
